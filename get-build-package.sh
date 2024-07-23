11f741e2b5f72dca533242e87f317cf6de48d310
NOURELDEEN ELSAYED <79816069+xoureldeen@users.noreply.github.com>
2024-07-23 16:36:59 +0300

    Update get-build-package.sh

--- get-build-package.sh
+++ get-build-package.sh
@@ -4,7 +4,7 @@
 
 BRANCH="master"
 
-git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/termux/termux-packages.git
+git clone --depth 1 -b ${BRANCH} --single-branch https://github.com/xoureldeen/termux-packages.git
 
 for i in build-package.sh clean.sh packages x11-packages root-packages scripts; do
 	rm -fr ./${i}
